c4c298e1ccdec590556ce4dba1cea0bba0baad9b
dj7777 <deepak_dj26@hotmail.com>
2017-01-16 15:12:38 +0530

    code updated

--- app.js
+++ app.js
@@ -10,6 +10,29 @@ fs.readFile('data.json', 'utf8', function (err, data) {
   obj = JSON.parse(data);
 });
 
+// import the language driver
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+ var ObjectID = require('mongodb').ObjectID;
+  // Connection URL
+var url = 'mongodb://127.0.0.1:27017/test';
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  
+  //ensure we've connected
+  assert.equal(null, err);
+  
+
+  console.log("Connected correctly to server");
+
+  var eventData = db.collection('event_data');
+
+//close the database connection
+  
+                return db.close();
+});
+
 var server = restify.createServer();   
 server.listen(process.env.port || process.env.PORT || 3978, function(){
     console.log('%s listening to %s', server.name, server.url);
